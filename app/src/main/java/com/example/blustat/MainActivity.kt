69be935c605a5ce0e4d93e4e622f0c882d5b4e8b
Ryan Jokuti <r.jokuti@gmail.com>
2020-04-06 21:16:43 -0500

    Working out how to manipulate text in the main view + worked on GATT

--- app/src/main/java/com/example/blustat/MainActivity.kt
+++ app/src/main/java/com/example/blustat/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.blustat
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.NullPointerException
+import com.example.blustat.DeviceIndexing.connectedDevices
+import kotlinx.coroutines.*
 
 // Vars
 private const val TAG = "MainActivity"
+var currentDevice = 0
+var startTime: Long = 0
 
 // Controls what the user sees
 // NO BLUETOOTH FUNCTIONS SHOULD BE CALLED FROM THIS CLASS
@@ -20,60 +24,86 @@ class MainActivity : AppCompatActivity() {
 
     // On start, establishes that the device is using Bluetooth and then moves onto display services
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.i(TAG, "Currently running function onCreate on ${Thread.currentThread().name}")
+        // Setting up receiver
+        val receiver = DeviceReceiver()
+        val filter = IntentFilter().apply {
+        addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)
+        }
+        registerReceiver(receiver, filter)
+
+        // Setting up content
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkBluetoothCompatibility()
+        /*
         val textDeviceName: TextView = findViewById(R.id.textDeviceName)
         val textDeviceUUID: TextView = findViewById(R.id.textDeviceUUID)
         val textDeviceType: TextView = findViewById(R.id.textDeviceType)
         val textDeviceBattery: TextView = findViewById(R.id.textDeviceBattery)
         val textDevicePing: TextView = findViewById(R.id.textDevicePing)
+         */
+
+
+
+
+        // Determine if you should run code or not
+        if (checkBluetoothCompatibility()) {
+            DeviceIndexing.refreshList()
+            //onDeviceChange()
+            GlobalScope.launch {
+                constantRefresh()
+            }
+        }
     }
 
     // This check to run though compatibility testing for a bluetooth device
-    private fun checkBluetoothCompatibility(){
-        Log.i(TAG, "Currently running function checkBluetoothCompatibility on ${Thread.currentThread().name}")
+    private fun checkBluetoothCompatibility(): Boolean{
         val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         // Check if device supports Bluetooth
         if (mBluetoothAdapter != null) {
-            Log.i(TAG, "Bluetooth supported on the device")
+            Log.i(TAG, "Device supports Bluetooth")
             // Check if Bluetooth is enabled
-            if (mBluetoothAdapter.isEnabled) {
+            return if (mBluetoothAdapter.isEnabled) {
                 Log.i(TAG, "Device has Bluetooth enabled")
                 // Start info display service
-                displayService()
+                true
             } else {
                 Log.w(TAG, "Device has Bluetooth disabled")
                 // Ask to enable Bluetooth
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, 2)
+                Toast.makeText(applicationContext, "Please restart the app after enabling Bluetooth", Toast.LENGTH_LONG).show()
+                false
             }
         } else {
             // Inform the user that Bluetooth isn't supported
             Log.e(TAG, "Device does not support Bluetooth")
             textDeviceName.text = resources.getString(R.string.unsupported_device)
             Toast.makeText(applicationContext, "This device does not support Bluetooth", Toast.LENGTH_LONG).show()
+            return false
         }
     }
 
-    // Display service for displaying information
-    fun displayService() {
-        Log.i(TAG, "Currently running function displayService on ${Thread.currentThread().name}")
-        try {
-            // Getting info about the selected Bluetooth device
-            val selectedDevice = DeviceIndexing.deviceIndex()[0]
-            val deviceType = StatRetrieval.getType(selectedDevice)
-            val currentPing = StatRetrieval.getPing(selectedDevice)
+    // Constantly updates info on the selected Bluetooth device
+    fun constantRefresh() {
 
-            // User end of viewing data
-            textDeviceName.text = ("${selectedDevice.name}")
-            textDeviceUUID.text = ("UUIDs: ${selectedDevice.uuids}")
-            textDeviceType.text = ("Type: $deviceType")
-            textDeviceBattery.text = ("Battery: N/A")
-            textDevicePing.text = ("Ping: $currentPing ms")
-        } catch (e: NullPointerException){
-            Log.w(TAG, "Exception thrown: $e")
+        while (true) {
+            var currentTime = System.currentTimeMillis()
+            if ((currentTime - startTime) > 5000) {
+                if (connectedDevices != null) {
+                    val deviceType = StatRetrieval.getType(connectedDevices[currentDevice])
+                    //Log.i(TAG, StatRetrieval.getBattery(this, connectedDevices[currentDevice]))
+                    val currentPing = StatRetrieval.getPing(connectedDevices[currentDevice])
+                    textDeviceBattery.text = ("Battery: N/A")
+                    textDevicePing.text = ("Ping: $currentPing ms")
+                    textDeviceName.text = ("${connectedDevices[currentDevice].name}")
+                    textDeviceUUID.text = ("UUIDs: ${connectedDevices[currentDevice].uuids}")
+                    textDeviceType.text = ("Type: $deviceType")
+                    startTime = System.currentTimeMillis()
+                    Log.i(TAG, "Time restarted")
+                }
+            } else {
+                //Log.i(TAG, (currentTime - startTime).toString())
+            }
         }
     }
 }
--- app/src/main/java/com/example/blustat/StatRetrieval.kt
+++ app/src/main/java/com/example/blustat/StatRetrieval.kt
@@ -1,17 +1,92 @@
 package com.example.blustat
 
 import android.bluetooth.*
+import android.content.Context
 import android.util.Log
+import java.util.*
+import kotlin.coroutines.coroutineContext
 
 // Vars
 private const val TAG = "StatRetrieval"
 
 // List of functions to get certain stats from a Bluetooth device
 object StatRetrieval {
+    private val gattCallback = object : BluetoothGattCallback() {
+
+        override fun onServicesDiscovered(
+            gatt: BluetoothGatt,
+            status: Int
+        ) {
+            Log.i(TAG, "Services discovered with status $status... attempting to read battery level")
+            Log.i(TAG, "Services: ${gatt.services}")
+            for (service in gatt.services) {
+                Log.i(TAG, "Service UUID: ${service.uuid}")
+                for (characteristic in service.characteristics) {
+                    Log.i(TAG, "Characteristic: ${gatt.readCharacteristic(characteristic)}")
+                }
+            }
+
+
+            /*
+
+            val Battery_Service_UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
+            val Battery_Level_UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb")
+
+            val batteryService = gatt.getService(Battery_Service_UUID)
+            if (batteryService == null) {
+                Log.w(TAG, "Battery service not found!")
+                return
+            }
+
+            val batteryLevel = batteryService.getCharacteristic(Battery_Level_UUID)
+            if (batteryLevel == null) {
+                Log.w(TAG, "Battery level not found!")
+                return
+            }
+
+            gatt.readCharacteristic(batteryLevel);
+            Log.v(TAG, "batteryLevel: " + gatt.readCharacteristic(batteryLevel));
+            */
+        }
+
+        override fun onConnectionStateChange(
+            gatt: BluetoothGatt,
+            status: Int,
+            newState: Int
+        ) {
+            when (newState) {
+                BluetoothProfile.STATE_CONNECTING -> {
+                    Log.i(TAG, "Connecting to GATT server of device ${gatt.device.name} with status $status")
+                }
+                BluetoothProfile.STATE_CONNECTED -> {
+                    Log.i(TAG, "Connected to GATT server of device ${gatt.device.name} with status $status")
+                    /*
+                    if (gatt.discoverServices()) {
+                        Log.i(TAG, "Remote service discovery started")
+                    } else {
+                        Log.w(TAG, "Remote service discovery not started")
+                    }
+
+                     */
+                }
+                BluetoothProfile.STATE_DISCONNECTING -> {
+                    Log.i(TAG, "Disconnecting from GATT server of device ${gatt.device.name} with status $status")
+                }
+                BluetoothProfile.STATE_DISCONNECTED -> {
+                    Log.i(TAG, "Disconnected from GATT server of device ${gatt.device.name} with status $status")
+                    if (status == 133) {
+                        Log.i(TAG, "Retrying connection...")
+                        gatt.connect()
+                    } else {
+                        gatt.close()
+                    }
+                }
+            }
+        }
+    }
 
     // Checks the ping from device to Bluetooth device by asking the Bluetooth device for its UUID
     fun getPing(device: BluetoothDevice): Long {
-        Log.i(TAG, "Currently running function bluetoothPing on ${Thread.currentThread().name}")
         val startTime = System.currentTimeMillis()
         device.fetchUuidsWithSdp()
         val endTime = System.currentTimeMillis()
@@ -34,5 +109,69 @@ object StatRetrieval {
             BluetoothClass.Device.Major.WEARABLE -> deviceType = "Wearable"
         }
         return deviceType
+    }
+
+    fun getBattery(context: Context, device: BluetoothDevice) : String {
+        var batteryResults = "N/A"
+        val batteryGattCallback = object : BluetoothGattCallback() {
+            override fun onServicesDiscovered(
+                gatt: BluetoothGatt,
+                status: Int
+            ) {
+                Log.i(TAG, "Services discovered with status $status... attempting to read battery level")
+                batteryResults = "made it this far"
+                val Battery_Service_UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
+                val Battery_Level_UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb")
+
+                val batteryService = gatt.getService(Battery_Service_UUID)
+                if (batteryService == null) {
+                    batteryResults = "Battery service not found"
+                    Log.w(TAG, batteryResults)
+                    return
+                }
+
+                val batteryLevel = batteryService.getCharacteristic(Battery_Level_UUID)
+                if (batteryLevel == null) {
+                    batteryResults = "Battery level not found"
+                    Log.w(TAG, batteryResults)
+                    return
+                }
+
+                gatt.readCharacteristic(batteryLevel)
+                return
+            }
+
+            override fun onCharacteristicRead(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+            ) {
+                batteryResults = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0).toString()
+                return
+            }
+
+            override fun onConnectionStateChange(
+                gatt: BluetoothGatt,
+                status: Int,
+                newState: Int
+            ) {
+                when (newState) {
+                    BluetoothProfile.STATE_CONNECTED -> {
+                        Log.i(TAG, "Connected to GATT server of device ${gatt.device.name} with status $status, discovering services...")
+                        if (gatt.discoverServices()) {
+                            Log.i(TAG, "Remote service discovery has been started")
+                        } else {
+                            Log.w(TAG, "Remote service discovery failed to start")
+                            return
+                        }
+                    }
+                    BluetoothProfile.STATE_DISCONNECTED -> {
+                        Log.i(TAG, "Disconnected from GATT server of device ${gatt.device.name} with status $status")
+                    }
+                }
+            }
+        }
+        val bluetoothGatt = device.connectGatt(context, false, batteryGattCallback)
+        return batteryResults
     }
 }
